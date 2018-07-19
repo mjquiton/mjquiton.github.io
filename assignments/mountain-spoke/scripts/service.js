67437459ffa95583ada1951eb7a492b6a26e04d9
Melissa Quiton <qui12028@byui.edu>
2018-07-18 19:40:40 -0700

    adventure tour page

---
+++ assignments/mountain-spoke/scripts/service.js
@@ -0,0 +1,60 @@
+let bike = new XMLHttpRequest();
+let bikeURL = 'https://mjquiton.github.io/assignments/mountain-spoke/data/data-ms.json';
+
+bike.open('GET', bikeURL, true);
+bike.responseType = 'json';
+bike.send();
+
+bike.onload = function(){
+   var bikeinfo = bike.response;
+    
+    var quickbike = bikeinfo.bikes[0].name;
+    document.getElementById("qTune").innerHTML = quickbike;
+    
+    var quickprice = bikeinfo.bikes[0].name;
+    document.getElementById("qPrice").innerHTML = quickprice;
+    
+    var quickdesc = bikeinfo.bikes[0].description;
+    document.getElementById("qDesc").innerHTML = quickdesc;
+    
+    var frankrain = towninfo.towns[0].averageRainfall;
+    document.getElementById("frainFall").innerHTML = frankrain;
+    
+    var frankevents = towninfo.towns[0].events;
+    document.getElementById("fevents").innerHTML = frankevents;
+    
+    
+    
+    var greenmotto = towninfo.towns[1].motto;
+    document.getElementById("gmotto").innerHTML = greenmotto;
+    
+    var greenyear = towninfo.towns[1].yearFounded;
+    document.getElementById("gyear").innerHTML = greenyear;
+    
+    var greenpop = towninfo.towns[1].currentPopulation;
+    document.getElementById("gpopulation").innerHTML = greenpop;
+    
+    var greenrain = towninfo.towns[1].averageRainfall;
+    document.getElementById("grainFall").innerHTML = greenrain;
+    
+    var greenevents = towninfo.towns[1].events;
+    document.getElementById("gevents").innerHTML = greenevents;
+    
+    
+    
+    var springmotto = towninfo.towns[3].motto;
+    document.getElementById("smotto").innerHTML = springmotto;
+    
+    var springyear = towninfo.towns[3].yearFounded;
+    document.getElementById("syear").innerHTML = springyear;
+    
+    var springpop = towninfo.towns[3].currentPopulation;
+    document.getElementById("spopulation").innerHTML = springpop;
+    
+    var springrain = towninfo.towns[3].averageRainfall;
+    document.getElementById("srainFall").innerHTML = springrain;
+    
+    var springevents = towninfo.towns[3].events;
+    document.getElementById("sevents").innerHTML = springevents;
+    
+}
